aa1088ac360b13a9d554d0edf24cc8d9f14116a9
Greg Gershman <greg@shelrick.com>
2014-04-06 16:30:35 -0400

    One more working copy.

--- lib/xml_security.rb
+++ lib/xml_security.rb
@@ -114,7 +114,7 @@ module XMLSecurity
       cert                    = OpenSSL::X509::Certificate.new(cert_text)
 
       # signature method
-      signature_algorithm     = algorithm(REXML::XPath.first(signed_info_element, "//ds:SignatureMethod", {"ds"=>DSIG}))
+      signature_algorithm     = algorithm(REXML::XPath.first(@working_copy, "//ds:SignatureMethod", {"ds"=>DSIG}))
 
       unless cert.public_key.verify(signature_algorithm.new, signature, canon_string)
         return soft ? false : (raise Onelogin::Saml::ValidationError.new("Key validation error"))
